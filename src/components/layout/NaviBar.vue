76a64b12e6b10d4fa160be329d71728d593396ba
wansang93 <wansang93@naver.com>
2022-05-22 14:34:46 +0900

    add HomeView

---
+++ src/components/layout/NaviBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-toolbar flat>
+      <v-toolbar-title> HappyHouse </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>home</v-btn>
+        <v-btn text>MAP</v-btn>
+        <v-btn text>BOARD</v-btn>
+        <v-btn text>QnA</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HeaderNaviBar",
+
+  data: () => ({}),
+};
+</script>
+
+<style></style>
