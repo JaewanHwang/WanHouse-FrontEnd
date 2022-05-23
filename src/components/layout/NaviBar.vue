ee559d95ae115e7fb30471ea4a4754b905d65f31
wansang93 <wansang93@naver.com>
2022-05-23 15:48:21 +0900

    modify navbar

--- src/components/layout/NaviBar.vue
+++ src/components/layout/NaviBar.vue
@@ -1,33 +1,22 @@
 <template>
-  <v-container>
-    <v-toolbar flat>
-      <v-col>
-        <v-toolbar-title>
-          Happy House
-          <!-- <v-img src="@/assets/logo.png" max-height="80" max-width="80"></v-img> -->
-        </v-toolbar-title>
-      </v-col>
-      <v-col>
-        <v-toolbar-items>
-          <router-link :to="{ name: 'home' }" class="link">
-            <button text>home</button>
-          </router-link>
-          <router-link :to="{ name: 'user' }" class="link">
-            <button text>user</button>
-          </router-link>
-          <router-link :to="{ name: 'map' }" class="link">
-            <button text>map</button>
-          </router-link>
-          <router-link :to="{ name: 'board' }" class="link">
-            <button text>board</button>
-          </router-link>
-          <router-link :to="{ name: 'qna' }" class="link">
-            <button text>qna</button>
-          </router-link>
-        </v-toolbar-items>
-      </v-col>
-    </v-toolbar>
-  </v-container>
+  <div>
+    <h1>HappyHouse</h1>
+    <router-link :to="{ name: 'home' }" class="link">
+      <button text>home</button>
+    </router-link>
+    <router-link :to="{ name: 'user' }" class="link">
+      <button text>user</button>
+    </router-link>
+    <router-link :to="{ name: 'map' }" class="link">
+      <button text>map</button>
+    </router-link>
+    <router-link :to="{ name: 'board' }" class="link">
+      <button text>board</button>
+    </router-link>
+    <router-link :to="{ name: 'qna' }" class="link">
+      <button text>qna</button>
+    </router-link>
+  </div>
 </template>
 
 <script>
