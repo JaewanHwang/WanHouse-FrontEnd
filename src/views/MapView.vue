4717ecb248865c7a5a0578d35e62ce2b18121050
wansang93 <wansang93@naver.com>
2022-05-23 15:25:44 +0900

    test user

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: "HomeView",
+  name: "MapView",
   props: {
     msg: String,
   },
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>유저</h1>
+  <div>
+    <form>
+      <label for="userid">아이디</label>
+      <input type="text" id="userid" ref="userid" v-model="userid" />
+      <button @click.prevent="sendId">중복확인</button>
+      <br />
+      <label for="username">이름</label>
+      <input type="text" id="username" ref="username" v-model="username" />
+      <br />
+      <button @click.prevent="checkVal">등록</button>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -7,6 +18,39 @@ export default {
   name: "HomeView",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      userid: "",
+      username: "",
+    };
+  },
+  methods: {
+    checkVal() {
+      if (!this.userid) {
+        alert("아이디를 입력하세요!!");
+        this.$refs.userid.focus();
+        return;
+      } else if (!this.username) {
+        alert("이름을 입력하세요!!");
+        this.$refs.username.focus();
+        return;
+      }
+      this.register();
+    },
+
+    register() {
+      alert("서버에 전송했엉~");
+    },
+
+    sendId() {
+      if (this.userid == "123") {
+        alert("중복이야~~");
+      } else {
+        alert("합격!");
+      }
+      return;
+    },
   },
 };
 </script>
